a16b9df0b43bcebea5ba46fa32d6a5cfd944394f
KM-Akashi <komi_se127@hotmail.com>
2020-02-04 00:08:19 +0800

    [FIX] Fix some bugs of PPJoin

--- src/main/scala/cn/edu/kust/komi/ppjoinplus/PPJoinPlus.scala
+++ src/main/scala/cn/edu/kust/komi/ppjoinplus/PPJoinPlus.scala
@@ -1,80 +1,81 @@
 package cn.edu.kust.komi.ppjoinplus
 
-import cn.edu.kust.komi.ppjoinplus.models.Records
+import cn.edu.kust.komi.ppjoinplus.models.Record
 
 import scala.collection.mutable
-import scala.math.min
+import scala.collection.mutable.ListBuffer
+import scala.math._
 
-object PPJoinPlus {
-  def ppjoin(records: Records, t: Double): Unit = {
-    var S: Set[(Int, Int)] = Set[(Int, Int)]()
-    //      ppjoin(records, t)
-    val I: mutable.HashMap[String, Set[(Int, Int)]] = mutable.HashMap[String, Set[(Int, Int)]]()
-    for ((record, recordId) <- records.recordList.zipWithIndex) {
-      val A: mutable.HashMap[Int, Int] = mutable.HashMap[Int, Int]()
-      val px = record.prefix(t)
-      for ((token, tokenIndex) <- record.tokens.take(px).zipWithIndex) {
-        I.getOrElseUpdate(token, Set[(Int, Int)]())
-          .filter {
-            case (targetRecordId, _) => records.recordList(targetRecordId).tokens.length >= t * record.tokens.length
-          }
-          .foreach {
-            case (targetRecordId, tokenIndex) =>
-              val unbound = 1 + min(record.tokens.length - tokenIndex, records.recordList(targetRecordId).tokens.length - tokenIndex)
-              if (A.getOrElseUpdate(targetRecordId, 0) + unbound >= record.alphaWith(records.recordList(targetRecordId), t))
-                A(targetRecordId) = A(targetRecordId) + 1
-              else
-                A(targetRecordId) = 0
-          }
-        I(token) = I(token) + ((recordId, tokenIndex))
-      }
-      println(recordId, record, A)
-      //      verify(record, A, alpha)
-      for (relevantRecordID <- A.keySet) {
-        val py = records.recordList(relevantRecordID).prefix(t)
-        val alpha = record.alphaWith(records.recordList(relevantRecordID), t)
-        if (px < py) {
-          val ubound = A(relevantRecordID) + record.tokens.length - px
-          if (ubound >= alpha)
-            A(relevantRecordID) = record.tokens.length + records.recordList(relevantRecordID).tokens.length - px
-        }
-        else {
-          val ubound = A(relevantRecordID) + records.recordList(relevantRecordID).tokens.length - py
-          if (ubound >= alpha)
-            A(relevantRecordID) = record.tokens.length + records.recordList(relevantRecordID).tokens.length - py
-        }
-        if (A(relevantRecordID) > alpha)
-          S = S + ((recordId, relevantRecordID))
-      }
-    }
-    println("I", I)
-    println("S", S)
-    //    val I: mutable.HashMap[String, ListBuffer[(Int, Int)]] = mutable.HashMap[String, ListBuffer[(Int, Int)]]()
-    //
-    //    for ((record, id) <- records.recordList.zipWithIndex) {
-    //      val A: mutable.HashMap[Int, Int] = mutable.HashMap[Int, Int]()
-    //      val p = record.tokens.length - (t * record.tokens.length).ceil.toInt + 1
-    //      for ((token, index) <- record.tokens.zipWithIndex) {
-    //        I.getOrElseUpdate(token, new ListBuffer[(Int, Int)]()).foreach {
-    //          case (y, j) => {
-    //            if (records.recordList(y).tokens.length >= record.tokens.length * t) {
-    //              val unbound = 1 + min(record.tokens.length - index, records.recordList(y).tokens.length - j)
-    //              val alpha = ((t / (1 + t)) * (records.recordList(y).tokens.length + record.tokens.length)).ceil
-    //              if (A.keySet.contains(y) && A(y) + unbound >= alpha) A(y) = A.getOrElse(y, 0) + 1
-    //              else A.put(y, 0)
-    //            }
-    //          }
-    //        }
-    //        I(token) += Tuple2(id, index)
-    //      }
-    //      println(id, " - ", record, " : ", A)
+class PPJoinPlus {
+  val records: mutable.ListBuffer[Record] = new mutable.ListBuffer[Record]
+  var threshold: Double = 0
+  var tokenCount: Map[String, Int] = Map[String, Int]()
 
-    //      verify(record, A, alpha)
-    //      for (y <- A.keySet) {
-    //        val wx = record.tokens(p)
-    //        val wy = records.recordList(y).tokens(p)
-    //        val O = A(y)
-    //      }
+  def addRecord(lines: String*): Unit = lines.foreach(records += new Record(_))
+
+  def init(t: Double = 0.8): Unit = {
+    println("> Initialization <")
+    threshold = t
+    tokenCount = records.flatMap(record => record.tokens).groupBy(identity).mapValues(_.length)
+    records.foreach(_.order(tokenCount, threshold))
+    println(this)
   }
 
+  def ppjoin(): Set[(Int, Int)] = {
+    println("> PPJoin <")
+
+    var S: Set[(Int, Int)] = Set[(Int, Int)]()
+
+    // Inverted indices token in prefix map: I
+    val I: mutable.HashMap[String, ListBuffer[(Int, Int)]] = new mutable.HashMap[String, ListBuffer[(Int, Int)]]()
+
+    records.zipWithIndex.foreach {
+      case (record, recordID) => {
+        // Candidates map: A [recordID, same token count in prefix]
+        val A: mutable.HashMap[Int, Int] = mutable.HashMap[Int, Int]()
+        record.tokens.take(record.prefix).zipWithIndex.foreach {
+          case (token, tokenIndex) => {
+            I.getOrElseUpdate(token, new ListBuffer[(Int, Int)]())
+              .filter {
+                case (recordId, _) => records(recordId).tokens.length >= threshold * record.tokens.length
+              }
+              .foreach {
+                case (invertedRecordId, invertedIndex) =>
+                  val unbound = min(record.tokens.length - tokenIndex, records(invertedRecordId).tokens.length - invertedIndex)
+                  if (A.getOrElseUpdate(invertedRecordId, 0) + unbound >= record.alphaWith(records(invertedRecordId), threshold))
+                    A(invertedRecordId) = A(invertedRecordId) + 1
+                  else
+                    A(invertedRecordId) = 0
+              }
+            I(token).append((recordID, tokenIndex))
+          }
+        }
+        // verify(record, A, alpha)
+        for (relevantRecordID <- A.keySet) {
+          val alpha = record.alphaWith(records(relevantRecordID), threshold)
+          val overlap = record.tokens.toSet.&(records(relevantRecordID).tokens.toSet).size
+          if (overlap >= alpha)
+            S += ((recordID, relevantRecordID))
+        }
+        printf("A[%d - %-10s]: %s\n", recordID, record.original, A.toList.mkString(" "))
+      }
+    }
+
+    // Result Log
+    println("###### Result ######")
+    println("Inverted indices:")
+    for ((k, l) <- I) printf("%s -> %s\n", k, l.mkString(" "))
+    printf("Threshold: %f. Verify:\n", threshold)
+    for ((x, y) <- S) printf("%d - %d => %1.3f\n", x, y, jaccard(records(x).tokens.toSet, records(y).tokens.toSet))
+
+    S
+  }
+
+  def jaccard(x: Set[Any], y: Set[Any]): Double = {
+    val a: Double = (x.&(y)).size
+    val b: Double = (x ++ y).size
+    a / b
+  }
+
+  override def toString: String = records.zipWithIndex.map { case (record, index) => "%05d ｜ [%-30s] <= [%-30s]".format(index, record.tokens.mkString(" "), record.original) }.mkString("\n")
 }
--- src/main/scala/cn/edu/kust/komi/ppjoinplus/models/Record.scala
+++ src/main/scala/cn/edu/kust/komi/ppjoinplus/models/Record.scala
@@ -1,24 +1,15 @@
 package cn.edu.kust.komi.ppjoinplus.models
 
 import scala.collection.mutable
-import scala.collection.mutable.ListBuffer
 
-class Record(val line: String) {
+class Record(line: String) {
   val original: String = line
+  var prefix: Int = 0
   var tokens: List[String] = normalize()
 
   def normalize(): List[String] = {
-    //    val wordCount = line.split(" ").filter(_.nonEmpty).groupBy(identity).mapValues(_.length)
-    //    val tokenBuffer = new ListBuffer[String]
-    //    wordCount.foreach {
-    //      case (k, v) => {
-    //        for (i <- 0 until v)
-    //          if (i == 0) tokenBuffer += k
-    //          else tokenBuffer += k.concat(i.toString)
-    //      }
-    //    }
     val wordCount = new mutable.HashMap[String, Int]()
-    val tokenBuffer = new ListBuffer[String]
+    val tokenBuffer = new mutable.ListBuffer[String]
     line.split(" ").filter(_.nonEmpty).foreach {
       word => {
         wordCount.getOrElseUpdate(word, 0) match {
@@ -31,17 +22,15 @@ class Record(val line: String) {
     tokenBuffer.toList
   }
 
-  def order(statistics: Map[String, Int]): Unit = {
+  def order(statistics: Map[String, Int], t: Double): Unit = {
     tokens = tokens.sortBy(word => statistics(word))
+    prefix = tokens.length - (t * tokens.length).ceil.toInt + 1
   }
 
-  def prefix(t: Double): Int = {
-    tokens.length - (t * tokens.length).ceil.toInt + 1
-  }
+  //  def prefix(t: Double): Int = tokens.length - (t * tokens.length).ceil.toInt + 1
 
-  def alphaWith(target: Record, t: Double): Int = {
-    ((t / (1 + t)) * (tokens.length + target.tokens.length)).ceil.toInt
-  }
+  def alphaWith(target: Record, t: Double): Int = ((t / (1 + t)) * (tokens.length + target.tokens.length)).ceil.toInt
 
   override def toString: String = tokens.toString()
+
 }
